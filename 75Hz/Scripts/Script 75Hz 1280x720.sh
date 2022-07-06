3a801a5cca54d5d77b11f861cebc6e03225a0185
wachin <wachin.id@gmail.com>
2022-07-06 12:05:44 -0500

    Guardar automáticamente el commit P1 mié 06 jul 2022 12:05:43 -05

--- 75Hz/Scripts/Script 75Hz 1280x720.sh
+++ 75Hz/Scripts/Script 75Hz 1280x720.sh
@@ -1,34 +1,39 @@
-#! /bin/bash
-
-# Script para Ubuntu 17.04
-# Este es el script que yo utilizo para ponerle 75Hz a mi monitor externo y al mismo
-# Adaptado de "Using xrandr and gtf to add a new mode to your X configuration at runtime" http://www.arunviswanathan.com/node/53
-# Este script funciona primero apagando el monitor de mi laptop y luego poniendo el nuevo modo al externo y los 75Hz
-
-xrandr --newmode "1280x768_75.00"  102.98  1280 1360 1496 1712  768 769 772 802  -HSync +Vsync
-xrandr --addmode VGA-1 1280x768_75.00
-xrandr --output VGA-1 --mode 1280x768_75.00
-
-
-# Nota: Para compartir este script con otras personas, primero hay que probarlo desde la linea dos "--addmode". Esta linea a veces no funciona pues el comando pone el nombre del monitor externo y depende del sistema operativo linux este le dará uno u otro nombre, así por ejemplo, yo usaba UbuntuStudio 16.04 y luego me cambié al nuevo 17.04 y no me funcionaba el script y al poner en la terminal esa linea salió esto:
-
-# wachin@wachin-id:~$ xrandr --addmode VGA1 1280x768_75.00
-# xrandr: cannot find output "VGA1"
-
-# así que para que funcione puse en la terminal: xrandr y el nombre del monitor externo estaba diferente(copio aquí una parte):
-
-#   400x300       60.32    56.34  
-#   320x240       60.05  
-# VGA-1 connected 1366x768+0+0 (normal left inverted right x axis y axis) 410mm x 230mm
-#   1366x768      59.79*+
-#   1280x1024     75.02    60.02  
-
-# Así que en base a esto actualicé esa linea así:
-
-# xrandr --addmode VGA-1 1280x768_75.00
-
-# e hice lo mismo con la siguiente linea
 
 
 
+Cómo crear Script para añadir 75Hz en Monitor Externo en Linux
 
+Primero, según esta página:
+
+http://www.arunviswanathan.com/node/53
+
+Ingresé los datos a la terminal:
+
+wachin@wachin-Inspiron-1750:~$ gtf 1280 720 75
+
+ # 1280x720 @ 75.00 Hz (GTF) hsync: 56.40 kHz; pclk: 95.65 MHz
+  Modeline "1280x720_75.00"  95.65  1280 1352 1488 1696  720 721 724 752  -HSync +Vsync
+
+ (Explicación wachín, como se observa en el siguiente script toda esa linea desde 12.... script según la página de arriba)
+wachin@wachin-Inspiron-1750:~$ xrandr --newmode "1280x720_75.00"  95.65  1280 1352 1488 1696  720 721 724 752  -HSync +Vsync
+wachin@wachin-Inspiron-1750:~$ xrandr --addmode VGA-1 1280x720_75.00
+wachin@wachin-Inspiron-1750:~$ xrandr --output VGA-1 --mode 1280x720_75.00
+wachin@wachin-Inspiron-1750:~$
+
+Luego de darme cuenta de los valores necesarios para añadir el nuevo modo, esto es lo que pondré en el script:
+
+
+---------------------------------------------------------
+xrandr --newmode "1024x768_75.00"  81.80  1024 1080 1192 1360  768 769 772 802  -HSync +Vsync
+xrandr --addmode VGA1 1024x768_75.00
+xrandr --output VGA1 --mode 1024x768_75.00
+-------------------------------------------------------------
+
+Sistema Operativo: Kbuntu 12.04.2
+
+
+Siguiendo las instrucciones de:
+
+
+
+                   
